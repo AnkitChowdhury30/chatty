62d8788c4f57ba4a661369d4d9ed34f40a6ce1b7
Ankit Chowdhury <ankitchowdhury550@gmail.com>
2025-01-31 22:42:31 +0530

    fixed typo in uploading profile pic

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -85,7 +85,7 @@ export const updateProfile = async (req, res) => {
     const uploadResponse = await cloudinary.uploader.upload(profilePic);
     const updatedUser = await User.findByIdAndUpdate(userId,{profilePic:uploadResponse.secure_url},{new:true});
 
-    res.status(200).json(updateUser);
+    res.status(200).json(updatedUser);
   } catch (error) {
     console.log("error in updateProfile controller",error);
     res.status(500).json({message:"Internal Server error"});
